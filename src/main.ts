cd670457d36c519516964ba4ebb712fd1e929349
Christian Borja <cpborja32@gmail.com>
2025-04-25 21:42:16 -0700

    refactor: move html code into new main.ts file

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,48 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import "./style.css";
+import Game from "../old/game.js";
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
+document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
+    <div id="game-container">
+        <div id="text-section">
+            <div id="game-text"></div>
+                <div id="input-container">
+                    <span class="prompt">></span>
+                    <input type="text" id="command-input" autofocus />
+                </div>
+        </div>
+
+        <div id="game-content">
+                <div id="pixel-art-container">
+                    <canvas id="pixel-art"></canvas>
+                    <div class="battle-ui">
+                        <div class="health-display enemy-health">
+                            <div class="health-text">
+                                <span class="name">ENEMY</span>
+                                <span class="hp">HP: 50/50</span>
+                            </div>
+                            <div class="health-bar">
+                                <div
+                                    class="health-bar-fill"
+                                    style="width: 100%"
+                                ></div>
+                            </div>
+                        </div>
+                        <div class="health-display player-health">
+                            <div class="health-text">
+                                <span class="name">PLAYER</span>
+                                <span class="hp">HP: 100/100</span>
+                            </div>
+                            <div class="health-bar">
+                                <div
+                                    class="health-bar-fill"
+                                    style="width: 100%"
+                                ></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+  `;
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+const game = new Game();
