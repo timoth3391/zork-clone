3b09ea7d32e949839899acadf556720151110aff
Christian Borja <cpborja32@gmail.com>
2025-05-03 16:15:38 -0700

    feat: Player class now needs an currentRoom when instantiated

--- src/entities/Player.ts
+++ src/entities/Player.ts
@@ -1,16 +1,19 @@
 import { Room } from "../lib/types";
+import { RoomName } from "../world/rooms";
+
+export type PlayerType = InstanceType<typeof Player>;
 
 const INITIAL_PLAYER_HEALTH = 100;
 
-export default class Player {
-    currentRoom: Room | null;
+export default class Player implements PlayerType {
+    currentRoom: RoomName | null;
     inventory: [];
     health: number;
     hasKey: boolean;
-    previousRoom: Room | null;
+    previousRoom: RoomName | null;
 
-    constructor() {
-        this.currentRoom = null;
+    constructor({ currentRoom }: { currentRoom: RoomName }) {
+        this.currentRoom = currentRoom;
         this.inventory = [];
         this.health = INITIAL_PLAYER_HEALTH;
         this.hasKey = false;
